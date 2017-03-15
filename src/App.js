0d73bfc1d917ac203b3a843112c89e1dacc9e079
areee <areee@users.noreply.github.com>
2017-03-16 00:05:04 +0200

    functionality ready

--- src/App.js
+++ src/App.js
@@ -14,27 +14,34 @@ class App extends Component {
       tf1: "Mouse was clicked: ",
       tf2: "Mouse moved: ",
       tf3: "Mouse wheel event: ",
+      w: 0,
     }
   }
 
   mouseMove = (event) => {
     let x = event.clientX;
     let y = event.clientY;
-
     var result = "Mouse moved: x: " + x + ", y: " + y;
-
 		this.setState({
 			tf2: result,
 		});
 	}
 
-  mouseDown = (event) =>{
-
-    var result = "Mouse was clicked: "+event.target.id;
-
-
+  mouseDown = (event) => {
+    var result = "Mouse was clicked: " + event.target.id;
     this.setState({
       tf1: result,
+    });
+  }
+
+  wheel = (event) => {
+    var result = this.state.w;
+    result = result+1;
+    var result1 = "Mouse wheel event: " + result + " clicks";
+
+    this.setState({
+      tf3: result1,
+      w: result,
     });
   }
 
@@ -43,15 +50,15 @@ class App extends Component {
   render() {
     return (
       <MuiThemeProvider>
-        <Paper style={{width: 400, margin: 20, padding: 20}} onMouseMove={this.mouseMove}>
+        <Paper style={{width: 300, margin: 20, padding: 20}} onMouseMove={this.mouseMove} onWheel={this.wheel} >
           <div onMouseDown={this.mouseDown}>
-            <TextField disabled={true} value={this.state.tf1} id="button1"/>
+            <TextField value={this.state.tf1} id="button1"/>
           </div>
           <div onMouseDown={this.mouseDown}>
-            <TextField disabled={true} value={this.state.tf2} id="button2"/>
+            <TextField value={this.state.tf2} id="button2"/>
           </div>
           <div onMouseDown={this.mouseDown}>
-            <TextField disabled={true} value={this.state.tf3} id="button3"/>
+            <TextField  value={this.state.tf3} id="button3"/>
           </div>
         </Paper>
       </MuiThemeProvider>
